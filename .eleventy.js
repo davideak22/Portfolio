ef626590716c1648f60d4628fd1654cee83f1654
davideak22 <165585199+davideak22@users.noreply.github.com>
2025-08-05 13:26:18 -0500

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+module. exports = function (eleventyConfig){
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/photo1.jpg");
+    eleventyConfig.addPassthroughCopy("./src/photo2.jpg");
+    eleventyConfig.addPassthroughCopy("./src/photo3.jpg");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/deakdavid-1428.jpg");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        // Can use toLocaleString the same way we were before
+        return dateObj.toLocaleString(undefined, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+      });
+      eleventyConfig.addFilter("projectDate", (dateObj) => {
+        // Can use toLocaleString the same way we were before
+        return dateObj.toLocaleString(undefined, {
+          year: "numeric",
+        });
+      });
+      
+    return{
+        dir: {
+            input: "src",
+            output: "public",
+            includes: "_includes"
+        },
+        markdownTemplateEngine: "njk", // allow Nunjucks in markdown
+        dataTemplateEngine: "njk",     // allow Nunjucks in .json/.11tydata files
+        htmlTemplateEngine: "njk"      // allow layouts to be njk
+    };
+};
