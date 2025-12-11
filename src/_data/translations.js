f2e5d9c5da7b8df07aef1507708acb20c40981a8
davideak22 <165585199+davideak22@users.noreply.github.com>
2025-12-11 22:35:06 +0000

    feat(translation): first implemantation

---
+++ src/_data/translations.js
@@ -0,0 +1,48 @@
+// src/_data/i18n/index.js
+module.exports = {
+  navbar: {
+    home: {
+      'en': 'Home',
+      'hu': 'Kezdőlap',
+      'ro': 'Acasă'
+    },
+    blog: {
+      'en': 'Blog',
+      'hu': 'Blog',
+      'ro': 'Blog'
+    },
+    projects: {
+      'en': 'Projects',
+      'hu': 'Projektek',
+      'ro': 'Proiecte'
+    },
+    aboutme: {
+      'en': 'About me',
+      'hu': 'Rólam',
+      'ro': 'Despre mine'
+    }
+  },
+  slugs: {
+    // The home page slug is intentionally empty, as it links to /en/ or /hu/
+    home: {
+      'en': '',
+      'hu': '',
+      'ro': ''
+    },
+    blog: {
+      'en': 'blog',
+      'hu': 'blog',
+      'ro': 'blog' 
+    },
+    projects: {
+      'en': 'projects',
+      'hu': 'projektek',
+      'ro': 'proiecte' 
+    },
+    aboutme: {
+      'en': 'about',
+      'hu': 'rolam',
+      'ro': 'despremine'
+    }
+  }
+}
